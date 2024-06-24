ae1e8d8547435ea615de75ad0dd4c23cc256fd73
BlueLight303 <bluelight3303@outlook.com>
2024-06-24 11:15:39 +0300

    UPDATE: updating the configuration

--- btcWallet.js
+++ btcWallet.js
@@ -5,36 +5,43 @@ const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 const axios = require('axios');
 
-// Define the network
-const network = bitcoin.networks.bitcoin; // use networks.testnet for testnet
+const createWallet = () => {
+    // Define the network
+    const network = bitcoin.networks.bitcoin; // use networks.testnet for testnet
 
-// Derivation path
-const path = `m/44'/0'/0'/0'`; // Use m/44'/1'/0'/0' for testnet
+    // Derivation path
+    const path = `m/44'/0'/0'/0'`; // Use m/44'/1'/0'/0' for testnet
 
-// Load your private key (WIF)
-let mnemonic = bip39.generateMnemonic()
-const seed = bip39.mnemonicToSeedSync(mnemonic)
-let root = bip32.fromSeed(seed, network)
+    // Load your private key (WIF)
+    let mnemonic = bip39.generateMnemonic()
+    const seed = bip39.mnemonicToSeedSync(mnemonic)
+    let root = bip32.fromSeed(seed, network)
 
-let account = root.derivePath(path)
-let node = account.derive(0).derive(0)
+    let account = root.derivePath(path)
+    let node = account.derive(0).derive(0)
+    // let btcAddress = bitcoin.payments.p2pkh({  // Address Format: Legacy Address (P2PKH),            Example: 1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2
+    // let btcAddress = bitcoin.payments.p2sh({   // Address Format: SegWit Address (P2SH) ,            Example: 3JvL6Ymt8MVWiCNHC7oWU6nLeHNJKLZGLN
+    // let btcAddress = bitcoin.payments.p2wpkh({ // Address Format: Native SegWit Address (bech32) ,   Example: bc1qqkvgqtpwq6g59xgwr2sccvmudejfxwyl8g9xg0
+    let btcAddress = bitcoin.payments.p2wpkh({
+        pubkey: node.publicKey,
+        network: network,
+    }).address;
 
-// let btcAddress = bitcoin.payments.p2pkh({  // Address Format: Legacy Address (P2PKH),            Example: 1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2
-// let btcAddress = bitcoin.payments.p2sh({   // Address Format: SegWit Address (P2SH) ,            Example: 3JvL6Ymt8MVWiCNHC7oWU6nLeHNJKLZGLN
-// let btcAddress = bitcoin.payments.p2wpkh({ // Address Format: Native SegWit Address (bech32) ,   Example: bc1qqkvgqtpwq6g59xgwr2sccvmudejfxwyl8g9xg0
-let btcAddress = bitcoin.payments.p2wpkh({
-    pubkey: node.publicKey,
-    network: network,
-}).address;
+    const wallet = {
+        address: btcAddress,
+        privateKey: node.toWIF(),
+        mnemonic: mnemonic
+    };
 
-console.log(` 
-    Wallet generated: 
-     - Address : ${btcAddress}, 
-     - Key : ${node.toWIF()}, 
-     - Mnemonic : ${mnemonic} 
-`);
+    console.log(`
+        Wallet generated:
+        - Address : ${wallet.address}, 
+        - Private Key : ${wallet.privateKey}, 
+        - Mnemonic : ${wallet.mnemonic} 
+    `);
 
-
+    return wallet;
+}
 
 const getBalance = async (address) => {
     try {
@@ -46,5 +53,8 @@ const getBalance = async (address) => {
     }
 };
     
-getBalance('bc1q0hpu8d4hunynvk9yttx9e74ftgl4mj5w49c9qp');
-getBalance('bc1qqkvgqtpwq6g59xgwr2sccvmudejfxwyl8g9xg0');
+// getBalance('bc1q0hpu8d4hunynvk9yttx9e74ftgl4mj5w49c9qp');
+// getBalance('bc1qqkvgqtpwq6g59xgwr2sccvmudejfxwyl8g9xg0');
+// getBalance('tb1qq8u98kq3hjsulerpuyj329vv8kk8amlc986aym');
+
+createWallet();
